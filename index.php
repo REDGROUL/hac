64bc397e752a8df40163f9a8aaa52e05ba59b041
RED <redgroulman@gmail.com>
2023-11-10 10:37:59 +0300

    простой шаблонизатор

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
+
+
 // Use this namespace
 use Steampixel\Route;
 
 Route::add('/', function() {
-    echo 'Welcome :-)';
+    $pager = new App\generatePage();
+    echo $pager->buildPage("auth.php");
 }, 'get');
 
 Route::add('/auth', function() {
---
+++ src/vars.php
@@ -0,0 +1,3 @@
+<?php
+
+$cvars['title'] = 'docs';
