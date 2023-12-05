5fd833e1d38f28cc3b855047d25fe57898d4e235
RED <redgroulman@gmail.com>
2023-12-05 18:44:28 +0300

    last commit

---
+++ src/views/main.blade.php
@@ -0,0 +1,14 @@
+@include('header')
+
+<div class="container mt-5">
+    <h1>Материалы для изучения</h1>
+    <ul class="list-group">
+        <li class="list-group-item">Учебник по HTML и CSS</li>
+        <li class="list-group-item">Видеокурс по JavaScript</li>
+        <li class="list-group-item">Статья о принципах адаптивного дизайна</li>
+        <li class="list-group-item">Книга по алгоритмам и структурам данных</li>
+    </ul>
+</div>
+
+
+@include('footer')
