a3ecbfd5469aff7b3e1b8a8567f1afdec92f0d19
RED <redgroulman@gmail.com>
2024-01-26 13:53:05 +0300

    [profile] добавлено отображение статуса задачи, убран лишний блок рядом с фото, добавил пробел между блоком с данными и блоком с редактированием данных

--- src/views/profile.blade.php
+++ src/views/profile.blade.php
@@ -1,10 +1,7 @@
 @include('header')
 
 <?
-    $um = new \App\Models\UserModel();
-
-
-
+$um = new \App\Models\UserModel();
 ?>
 <div class="container mt-4">
     <div class="row">
@@ -14,17 +11,14 @@
                 @if($userData['photo'] == null)
                     <img src="/res/images/nopic.jpg" class="card-img-top" alt="Фото профиля">
                 @else
-                    <img src= "{{$userData['photo']}}" class="mr-3 align-self-start profile-pic-mini" alt="Фото профиля" width="64" height="64">
+                    <img src="{{$userData['photo']}}" class="mr-3 align-self-start profile-pic-mini" alt="Фото профиля"
+                         width="64" height="64">
                 @endif
-
-                <div class="card-body">
-                    <h5 class="card-title"></h5>
                     <?$dm = new \App\Models\DepartmentModel();
-                        $dmName = $dm->getDepartmentNameById($userData['department']);
+                    $dmName = $dm->getDepartmentNameById($userData['department']);
 
                     ?>
-                    <h5 class="card-title"></h5>
-                </div>
+
             </div>
         </div>
 
@@ -34,83 +28,96 @@
                     <h5 class="card-title">ФИО: {{$userData['name']}}</h5>
                     <h5 class="card-title">Отдел: {{$dmName['name']}}</h5>
                     <?
-                        $rm = new \App\Models\RoleModel();
-                        $role = $rm->getRoleById($userData['role']);
+                    $rm = new \App\Models\RoleModel();
+                    $role = $rm->getRoleById($userData['role']);
 
                     ?>
                     <h5 class="card-title">Права: {{$role['name']}}</h5>
 
                 </div>
             </div>
-            <div class="card">
+            <br>
+            @if($_SESSION['uid'] == $userData['id'])
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Панель редактирования</h5>
+                        <!-- Добавьте элементы управления для редактирования профиля здесь -->
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+
+    <br>
+    <div class="card mb-3">
+        <div class="card-header bg-light">
+            <div class="header-task">
                 <div class="card-body">
-                    <h5 class="card-title">Панель редактирования</h5>
-                    <!-- Добавьте элементы управления для редактирования профиля здесь -->
+                    <h4>Текущие задачи:</h4>
                 </div>
             </div>
+
+            @foreach($tasks as $task)
+                <?
+                $stm = new \App\Models\TaskStatusModel();
+                $status = $stm->getStatusById($task['status']);
+                ?>
+
+                <div class="card comment_card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+
+                                <h5><a href="/task/{{$task['id']}}">{{$task['name']}}</a></h5>
+
+                                <span class="badge bg-{{$status['color']}} text-white mb-2"> {{$status['name']}}</span>
+
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                Описание: {{$task['description']}}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                Время сдачи: {{$task['date']}}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+                                <?
+                                $owner = $um->getUserById($task['owner_id'])
+                                ?>
+                                Постановщик: <a href="/profile/{{$owner['id']}}">{{$owner['name']}}</a>
+                            </div>
+                        </div>
+
+
+                        <div class="row">
+                            <div class="col">
+                                <?
+                                $worker = $um->getUserById($task['worker_id'])
+                                ?>
+                                Ответственный: <a href="/profile/{{$worker['id']}}">{{$worker['name']}}</a>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col">
+
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
-
-
-    <div class="card margins">
-        <div class="card-body">
-         <h4>Список задач:</h4>
-    </div>
-    </div>
-
-    @foreach($tasks as $task)
-
-
-    <div class="card comment_card">
-        <div class="card-body">
-            <div class="row">
-                <div class="col">
-                   Название: {{$task['name']}}
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                   Описание: {{$task['description']}}
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col">
-                   Время сдачи: {{$task['date']}}
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col">
-                    <?
-                    $owner = $um->getUserById($task['owner_id'])
-                    ?>
-                    Постановщик: <a href="/profile/{{$owner['id']}}">{{$owner['name']}}</a>
-                </div>
-            </div>
-
-
-            <div class="row">
-                <div class="col">
-                    <?
-                    $worker = $um->getUserById($task['worker_id'])
-                    ?>
-                    Ответственный: <a href="/profile/{{$worker['id']}}">{{$worker['name']}}</a>
-                </div>
-            </div>
-
-            <div class="row">
-                <div class="col">
-
-                    <a href="/task/{{$task['id']}}">К задаче</a>
-                </div>
-            </div>
-
-
-
-
-        </div>
-    </div>
-    @endforeach
 </div>
 @include('footer')
