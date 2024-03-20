390115c6253ae22feb7b59c7a9ae2e9ad8b9bd94
RED <redgroulman@gmail.com>
2024-03-20 09:59:39 +0300

    переработка api.php

--- api.php
+++ api.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\BoardsController;
 use App\Controllers\CommentController;
+use App\Controllers\DepartmentController;
 use App\Controllers\TaskController;
 use App\Controllers\UserController;
 use App\Models\DepartmentModel;
@@ -14,60 +15,78 @@ Route::add('/login', function (){
     echo $uc->login();
 }, 'post');
 
-Route::add('/tasks/changeStatus', function (){
-    $tc = new TaskController();
-    echo $tc->changeStatus();
-}, 'post');
+if($_SESSION['auth']) {
 
-Route::add('/tasks/newTask', function (){
-    $tc = new TaskController();
-    echo $tc->newTask();
-}, 'post');
+    Route::add('/tasks/changeStatus', function (){
+        $tc = new TaskController();
+        echo $tc->changeStatus();
+    }, 'post');
 
-Route::add('/addNewComment', function (){
-    $CommentContr = new CommentController();
-    echo $CommentContr->addNewComment();
-}, 'post');
+    Route::add('/tasks/newTask', function (){
+        $tc = new TaskController();
+        echo $tc->newTask();
+    }, 'post');
 
-Route::add('/addBoard', function (){
-    $boardController = new BoardsController();
-    echo $boardController->addBoard();
-}, 'post');
+    Route::add('/addNewComment', function (){
+        $CommentContr = new CommentController();
+        echo $CommentContr->addNewComment();
+    }, 'post');
 
-Route::add('/delboard', function (){
-    $boardController = new BoardsController();
-    echo $boardController->delBoard();
-}, 'post');
+    Route::add('/addBoard', function (){
+        $boardController = new BoardsController();
+        echo $boardController->addBoard();
+    }, 'post');
 
-Route::add('/delTask/([0-9-]*)', function ($id){
-    $tc = new TaskController();
-    echo $tc->delTaskById($id);
-}, 'post');
+    Route::add('/delboard', function (){
+        $boardController = new BoardsController();
+        echo $boardController->delBoard();
+    }, 'post');
 
-Route::add('/addUser', function (){
-    $json = json_decode(file_get_contents("php://input"), true);
-    $um = new UserModel();
-    $um->addUser($json['login'], $json['pass'], $json['name'], $json['role'], $json['dep']);
+    Route::add('/delTask/([0-9-]*)', function ($id){
+        $tc = new TaskController();
+        echo $tc->delTaskById($id);
+    }, 'post');
+
+    Route::add('/addUser', function (){
+        $um = new UserController();
+        echo $um->addUser();
+    }, 'post');
+
+    Route::add('/addDep', function (){
+        $dm = new DepartmentController();
+        echo $dm->createDep();
+    }, 'post');
+
+    Route::add('/getUserByDep/([0-9-]*)', function ($id){
+        $um = new UserModel();
+        $data = $um->getUserByDepId($id);
+        echo json_encode($data);
+    }, 'get');
+
+    Route::add('/updateTask', function (){
+        $json = json_decode(file_get_contents("php://input"), true);
+        $tm = new TasksModel();
+
+        $tm->updateTask($json);
+
+    }, 'post');
 
 
-}, 'post');
+} else{
+    http_send_status(401);
+}
 
-Route::add('/addDep', function (){
-    $json = json_decode(file_get_contents("php://input"), true);
-    $dm = new DepartmentModel();
-    $dm->createDep($json);
-}, 'post');
 
-Route::add('/getUserByDep/([0-9-]*)', function ($id){
-    $um = new UserModel();
-    $data = $um->getUserByDepId($id);
-    echo json_encode($data);
-}, 'get');
 
-Route::add('/updateTask', function (){
-    $json = json_decode(file_get_contents("php://input"), true);
-    $tm = new TasksModel();
 
-    $tm->updateTask($json);
 
-}, 'post');
+
+
+
+
+
+
+
+
+
+
