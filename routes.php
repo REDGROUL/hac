69c8c97d11bd0082e5ac98aaccfda16740fe64c6
RED <redgroulman@gmail.com>
2024-03-21 17:10:07 +0300

    cleanUp

--- routes.php
+++ routes.php
@@ -3,11 +3,8 @@
 use App\Controllers\DepartmentController;
 use App\Controllers\KanbanController;
 use App\Controllers\UserController;
-use App\Models\TasksModel;
 use Steampixel\Route;
-use App\Models\DepartmentModel;
 use App\Controllers\TaskController;
-$blade = new Jenssegers\Blade\Blade('src/views','src/cache');
 session_start();
 
 if(isset($_SESSION['auth'])) {
