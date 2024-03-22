61e3cc5e5df0e1205da7530b8bc094d28413f3df
RED <redgroulman@gmail.com>
2024-03-22 17:18:03 +0300

    удаление старой авторизации

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -28,55 +28,6 @@ class UserController extends BaseController
     }
 
 
-    public function login()
-    {
-        $json = $this->getInput();
-
-        $res = $this->userModel->getUser($json['login'], $json['pass']);
-
-        if(!empty($res))
-        {
-
-            $key = "some_keyhehe";
-            $time = time();
-            $access = [
-                'iat' => $time,
-                'exp' => time()+36000,
-                'login'=>$res['login'],
-                'name'=>$res['name'],
-                'type'=>'access',
-                'user_id'=>$res['id'],
-                'role'=>$res['role']
-
-            ];
-            $ref = [
-                'type'=>'refresh',
-                'user_id'=>$res['id']
-            ];
-            $token_acc = JWT::encode($access, $key, 'HS256');
-            $token_ref = JWT::encode($ref, $key, 'HS256');
-            session_start();
-            $_SESSION['uid'] = $res['id'];
-            $_SESSION['role'] = $res['role'];
-            $_SESSION['auth'] = true;
-            $_SESSION['dep'] = $res['department'];
-
-            return json_encode([
-                "status"=>"ok",
-                'access'=>$token_acc,
-                'refresh'=>$token_ref
-            ]);
-
-        }
-        else
-        {
-            return json_encode([
-                "status"=>"user not found"
-            ]);
-        }
-
-    }
-
     public function addUser() {
         $data = $this->getInput();
         $user = new User();
@@ -91,11 +42,11 @@ class UserController extends BaseController
     }
 
     public function getUserByDepId($id) {
-        return $this->userModel->getUserByDepId($id);
+        return $this->userService->findUserByDepartmentId($id);
     }
 
     public function getDataProfile($id) {
-        $currentUser = $this->userModel->getUserById($id);
+        $currentUser = $this->userService->getUserById($id);
         $tasks = $this->taskMode->getTaskByUserId($id);
         return $this->blade->make('profile', ['title'=>'Профиль','navbar_show'=>true, 'userData'=>$currentUser,
             'tasks'=>$tasks])->render();
