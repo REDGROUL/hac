3ad75f203c69af56dfe80a0f4026641eaebd5e7c
RED <redgroulman@gmail.com>
2024-03-22 17:16:36 +0300

    замена авторизации но новую

--- src/views/js/login.js
+++ src/views/js/login.js
@@ -1,37 +1,37 @@
-
-
 let loginbtn = document.getElementById('login');
-loginbtn.addEventListener("click", ()=> {
-    const form = document.getElementById('loginForm');
+loginbtn.addEventListener("click", () => {
     const login = document.getElementById('loginUsername').value;
     const pass = document.getElementById('loginPassword').value;
     console.log("click")
     let resp = JSON.stringify({
-        "type":"auth",
         "login": login,
-        "pass": pass});
+        "pass": pass
+    });
     ShowNotify("Менеджер авторизации", "Отправляем данные для проверки");
-    fetch('/login',{
+    fetch('/auth', {
         method: 'POST',
         body: resp,
 
     })
 
-        .then(response=>response.json())
+        .then(response => response.json())
 
-        .then(data=>{
-            if(data.status == "ok") {
-                localStorage.setItem('access', data['access']);
-                localStorage.setItem('refresh', data['refresh']);
+        .then(data => {
+
+
+            if (data.status === true) {
+                console.log(data)
+                localStorage.setItem("uid", data['uid'])
                 ShowNotify("Менеджер авторизации", "Успешая авторизация! \n " +
                     "Переводим на страницу задач", "success");
-                window.location ='/tasks';
+                window.location = '/tasks';
             } else {
+                console.log(data)
                 ShowNotify("Ошибка", "Пользователь не найден!", 'warning');
             }
 
         })
-        .catch(error=>{
+        .catch(error => {
             console.log(error);
             ShowNotify("Ошибка", error, 'alert');
         })
