d35e364f2f9ef07135f1067a179c321ace8b644f
RED <redgroulman@gmail.com>
2024-04-25 11:59:38 +0300

    Tasks: чистка

--- src/views/js/tasks.js
+++ src/views/js/tasks.js
@@ -1,30 +1,9 @@
+const dialog = document.querySelector(".example-dialog");
 
 
-let buttons = document.querySelectorAll('.addTask');
-buttons.forEach(function(button) {
-    button.addEventListener('click', function(event) {
-        // Ваш обработчик события и код здесь
-        let modal = document.getElementById('myModal');
-
-        // <input id="modal_board_id">
-        //         <input id="modal_owner_id">
-        //         <input id="modal_owner_id">
-
-        console.log(button.parentElement.id);
-        let modal_board_id = document.getElementById("modal_board_id");
-        let modal_owner_id = document.getElementById("modal_owner_id");
-        //let modal_worker_id = document.getElementById("modal_worker_id");
-
-        modal_board_id.value = event.target.id;
-        modal_owner_id.value = localStorage.getItem("uid");
-
-
-        var myModal = new bootstrap.Modal(modal);
-
-// Отобразите модальное окно
-        myModal.show();
-    });
-});
+document.getElementById("closeDialog").addEventListener("click", ()=>{
+    dialog.open = false;
+})
 
 let saveTask = document.getElementById("saveTask");
 
@@ -32,28 +11,6 @@ saveTask.addEventListener("click", ()=>{
     let form = document.getElementById('newTaskForm');
 
     let formData = new FormData(form);
-
-    // let name = document.getElementById("title").value;
-    // let description = document.getElementById("description").value;
-    // let modal_board_id = document.getElementById("modal_board_id").value;
-    // let modal_owner_id = document.getElementById("modal_owner_id").value;
-    // let date = document.getElementById("date").value;
-    // let worker = document.getElementById("worker").value;
-    // let stat = document.getElementById("status").value;
-    // console.log(date);
-    // console.log(worker);
-    // let resp = JSON.stringify({
-    //     "type":"newTask",
-    //     "name": name,
-    //     "description": description,
-    //     "kanban_id": modal_board_id,
-    //     "owner_id":modal_owner_id,
-    //     "worker_id": worker,
-    //     "date":date,
-    //     "status": stat
-    // });
-
-
 
     fetch('/tasks/newTasks',{
         method: 'POST',
@@ -68,13 +25,14 @@ saveTask.addEventListener("click", ()=>{
 
             console.log(data);
 
-
+            dialog.open = false;
             ShowNotify("Менеджер задач", "Сохраняем и обновляем", 'success');
+
             //
-            // setTimeout(function() {
-            //     // Перезагрузить текущую страницу
-            //     location.reload();
-            // }, 500);
+            setTimeout(function() {
+                // Перезагрузить текущую страницу
+                location.reload();
+            }, 500);
 
 
 
