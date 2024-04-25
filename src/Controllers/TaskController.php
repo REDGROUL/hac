bf967b3eae4b20186b7f413c6ec22225f634f7d7
RED <redgroulman@gmail.com>
2024-04-25 12:00:18 +0300

    tasks: удаление мусора

--- src/Controllers/TaskController.php
+++ src/Controllers/TaskController.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace App\Controllers;
+
 use App\Models\CommentsModel;
 use App\Models\TasksModel;
 use App\Models\TaskStatusModel;
 use App\Models\UserModel;
-use App\Security\Auth;
-use App\Security\SimpleSecurity;
+
 use App\Service\Impl\UserServiceImpl;
 use App\Service\Interfaces\UserService;
 use Jenssegers\Blade\Blade;
@@ -26,52 +26,54 @@ class TaskController extends BaseController
         $this->commentModel = new CommentsModel();
         $this->taskStatusModel = new TaskStatusModel();
         $this->userService = new UserServiceImpl();
-        $this->blade = new Blade('src/views','src/cache');
+        $this->blade = new Blade('src/views', 'src/cache');
     }
 
 
-    public function changeStatus() {
+    public function changeStatus()
+    {
         $data = $this->getInput();
         return $this->taskModel->changeStatus($data);
     }
 
-    public function newTask() {
-        var_dump($_POST);
+    public function newTask()
+    {
+
         return $this->taskModel->newTask();
     }
 
-    public function delTaskById($id) {
+    public function delTaskById($id)
+    {
         return json_encode($this->taskModel->delTaskById($id));
     }
 
-    public function updateTask() {
+    public function updateTask()
+    {
         $data = $this->getInput();
         echo json_encode($data);
         return $this->taskModel->updateTask($data);
     }
 
-    public function getTask($id) {
+    public function getTask($id)
+    {
         return $this->taskModel->getTask($id);
     }
 
-    public function getDetailTask($id) {
+    public function getDetailTask($id)
+    {
 
-            $data = $this->getTask($id)[0];
-            $comments = $this->commentModel->getCommentsByTaskId($data['id']);
+        $data = $this->getTask($id)[0];
+        $comments = $this->commentModel->getCommentsByTaskId($data['id']);
 
-            $data['comments'] = $comments;
-            $this->setRedisData("taskDetail_".$id, json_encode($data));
+        $data['comments'] = $comments;
+        $this->setRedisData("taskDetail_" . $id, json_encode($data));
 
-
-
-
-
-
-        return $this->blade->make('taskDetail', ['title'=>'Задача','navbar_show'=>true, 'task'=>$data,
-            'comments'=>$data['comments']])->render();
+        return $this->blade->make('taskDetail', ['title' => 'Задача', 'navbar_show' => true, 'task' => $data,
+            'comments' => $data['comments']])->render();
     }
 
-    public function getPrepareKanbanById($dep_id) {
+    public function getPrepareKanbanById($dep_id)
+    {
 
         $boards = $this->taskModel->getAllBoard();
         $tasks = $this->taskModel->getAllTasksByDepartment($dep_id);
@@ -79,10 +81,10 @@ class TaskController extends BaseController
         foreach ($tasks as $task) {
             $task['status'] = [
                 "name" => $stats[$task['status']]['name'],
-                "color" =>$stats[$task['status']]['color'],
+                "color" => $stats[$task['status']]['color'],
             ];
         }
         $users = $this->userService->findAllUser();
-        return $this->blade->make('tasks', ['title'=>'Задачи','navbar_show'=>true, 'boards'=>$boards, 'tasks'=>$tasks, 'users'=>$users, 'statuses'=>$stats, 'curent_dep'=>$dep_id])->render();
+        return $this->blade->make('tasks', ['title' => 'Задачи', 'navbar_show' => true, 'boards' => $boards, 'tasks' => $tasks, 'users' => $users, 'statuses' => $stats, 'curent_dep' => $dep_id])->render();
     }
 }
